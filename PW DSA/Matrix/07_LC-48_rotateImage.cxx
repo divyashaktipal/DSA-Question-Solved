2bdfc142c3f4562723228ce33c5dacd7136620c7
Divyashakti Pal <divyashaktipal@gmail.com>
2025-08-06 13:01:26 +0530

    Leetcode-48 rotate image

--- PW DSA/Matrix/07_LC-48_rotateImage.cxx
+++ PW DSA/Matrix/07_LC-48_rotateImage.cxx
@@ -1,10 +1,15 @@
-// Q.
+// Q. Rotate Image(LC-48)
 // You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
 // You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
 
 // Ex:
 // Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
 // Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+
+// Approach:
+// Calculate the transpose of the matrix.
+// swap the columns of transposed matrix using two pointer.
 
 #include <bits/stdc++.h>
 using namespace std;
